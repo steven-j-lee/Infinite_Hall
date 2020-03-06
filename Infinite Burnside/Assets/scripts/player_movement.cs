19508e77cb5346e488e75c0d4f41325fde167fe2
Steven Lee <koreancoffe@gmail.com>
2020-03-05 19:15:32 -0500

    added player movement

--- Infinite Burnside/Assets/scripts/player_movement.cs
+++ Infinite Burnside/Assets/scripts/player_movement.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class player_movement : MonoBehaviour
 {
+    private float speed = 5.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,22 @@ public class player_movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.forward * speed * Time.deltaTime;
+
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += Vector3.back * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
     }
 }
