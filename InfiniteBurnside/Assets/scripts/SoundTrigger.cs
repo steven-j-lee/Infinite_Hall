f225dd1e33d3a11e4e202cac794e9b23f6097f61
Samantha Difeo <sdifeo1@gmail.com>
2020-06-15 01:18:08 -0400

    delete lib and temp folders

---
+++ InfiniteBurnside/Assets/scripts/SoundTrigger.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundTrigger : MonoBehaviour
+{
+    [SerializeField] private AudioClip audioFile;
+    private AudioSource sound;
+    [SerializeField] float volume = 0f;
+    private bool hasPlayed = false;
+
+    //lights
+    [SerializeField] GameObject lights = null;
+
+
+    //audio to be muted
+    [SerializeField] private AudioClip audioFileToMute;
+    [SerializeField] private GameObject bgmMute;
+    private AudioSource soundToMute;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //get audio clip from entity
+        sound = GetComponent<AudioSource>();
+        soundToMute = bgmMute.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+ 
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!hasPlayed)
+        {
+            soundToMute.mute = true;
+            //sound.PlayOneShot(audioFile, volume);
+            hasPlayed = true;
+            Destroy(lights);
+        }
+
+    }
+
+}
---
+++ InfiniteBurnside/Assets/scripts/player_movement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_movement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpForce;
+    Rigidbody playerBody;
+    Vector3 orientation;
+    float jumpLimit = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerBody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+            Walk();
+    }
+
+
+    private bool IsGrounded()
+    {
+        //Debug.DrawRay(transform.position, Vector3.down * speed, Color.red);
+        return Physics.Raycast(transform.position, Vector3.down, jumpLimit);
+      
+    }
+
+    private void Walk()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 move = new Vector3(horizontal, 0, vertical) * speed * Time.fixedDeltaTime;
+
+        Vector3 newPosition = playerBody.position + playerBody.transform.TransformDirection(move);
+        playerBody.MovePosition(newPosition);
+    }
+
+
+}
