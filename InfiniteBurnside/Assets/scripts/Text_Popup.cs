c81790a6d2fbb8d7d57ba5c5d08d6440f0094f68
StevenPC <koreancoffe@gmail.com>
2020-06-25 22:30:20 -0400

    chair event added

--- InfiniteBurnside/Assets/scripts/Text_Popup.cs
+++ InfiniteBurnside/Assets/scripts/Text_Popup.cs
@@ -5,12 +5,20 @@ using TMPro;
 
 public class Text_Popup : MonoBehaviour
 {
-    [SerializeField] private TextMeshProUGUI key; 
+    [SerializeField] private TextMeshProUGUI key;
+    //[SerializeField] private GameObject knockDisable;
+    [SerializeField] private GameObject disableGravity;
+    [SerializeField] private GameObject chair;
+
+    private AudioSource sound;
+    private Rigidbody painting;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+       //sound = knockDisable.GetComponent<AudioSource>();
+        painting = disableGravity.GetComponent<Rigidbody>();
+
     }
 
     // Update is called once per frame
@@ -23,12 +31,18 @@ public class Text_Popup : MonoBehaviour
     {
         if(other.tag == "Player")
         {
-            key.SetText("A strange writings on the painting reads " + "\n" +
-                "Only for those who solve the riddle may pass");
+            key.SetText("Look Behind you");
+            StartCoroutine(Delay());
+            
         }
-        else
-        {
-            key.SetText(" ");
-        }
+    }
+
+    private IEnumerator Delay()
+    {
+        chair.transform.Rotate(90f, 0f, 0f);
+        yield return new WaitForSeconds(2f);
+        Destroy(key);
+        Destroy(disableGravity);
+        Destroy(painting);
     }
 }
