808fdcd7f3d4bf32ed11ad02319d04771560ea13
StevenPC <koreancoffe@gmail.com>
2020-07-06 14:51:22 -0400

    added audio

--- InfiniteBurnside/Assets/scripts/ProceduralStairs.cs
+++ InfiniteBurnside/Assets/scripts/ProceduralStairs.cs
@@ -21,16 +21,14 @@ public class ProceduralStairs : MonoBehaviour
             float yRot = lastStairs.transform.rotation.eulerAngles.y > 10 ? 0 : 180;
             Quaternion rot = Quaternion.Euler(new Vector3(0, yRot, 0));
             Vector3 pos = lastStairs.transform.position;
-            //pos.y -= 1.3f;
             pos.y -= 9.4f;
-            //pos.z = pos.z > 1 ? 0 : 7.3f;
             pos.z = pos.z > 1 ? 0 : 0.2f;
         
             if(stairsNum % 2 != 0)
             {
                 pos.x = pos.x > 1 ? 0 : 6.43f;
             }
-            else if(stairsNum % 2 == 0)
+            else
             {
                 pos.x = pos.x > 1 ? 0 : 0.43f;
             }
